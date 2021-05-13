346008a33e1d04dfa6e753d95be7790ec7df2174
unknown <rezadar1378@gmail.com>
2021-05-13 11:10:07 +0430

    added most wathced channels overall.py

---
+++ most watched channels overall.py
@@ -0,0 +1,53 @@
+import json
+import re
+import time
+
+# parameters
+N = 27199  # how many of the latest videos to check
+
+with open("watch-history.json", 'r', encoding="utf8") as f:
+    # measuring time
+    a = time.time_ns()
+
+    # loading data
+    data = json.load(f)
+    # how many videos have you watched
+    print(len(data), " videos watched")
+
+    # measuring time
+    b = time.time_ns()
+
+    # main process
+    channels = {}
+    for video in data[:N]:
+        # for deleted videos and other exceptions
+        if "subtitles" not in video:
+            continue
+
+        channel_name = video["subtitles"][0]["name"]
+        
+        if channel_name in channels:
+            channels[channel_name] += 1
+        else:
+            channels[channel_name] = 1
+
+    # sort channels by views
+    sorted_channels = {k: v for k, v in sorted(
+        channels.items(), key=lambda item: item[1], reverse=True)}
+    
+    # measuring time
+    print("loading took", int((b-a)/1000000), "ms")
+    print("sorting took", int((time.time_ns()-b)/1000000), "ms")
+
+    print()
+
+    print("searching last", N, "videos")
+
+    # print channels
+    # all_results = [(k, v) for k, v in sorted_channels.items()]
+
+
+    for i,item in enumerate(list(sorted_channels.items())[:1000]):
+        name, view = item
+        print(i+1,"-", name, view)
+    # [print(item[0],item[1]) for item in all_results[100]]
